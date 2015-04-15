9358b2dc17087fb582159095aee30b56a9bb7fa9
Hugo Nguyen <hugo@scribd.com>
2015-04-14 17:05:09 -0700

    first commit

---
+++ app/controllers/Analyzer.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.libs.Json;
+import play.mvc.Result;
+import play.mvc.Results;
+
+/**
+ * Created by hugo on 4/14/15.
+ */
+public class Analyzer {
+    public static Result getMetadata(String url) {
+        ObjectNode result = Json.newObject();
+
+        result.put("content", url);
+
+        return Results.ok(result);
+    }
+}
