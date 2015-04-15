5d14a6c219fdc41f479a3ae5f23cce37c7011315
Hugh Nguyen <hugh.hn@gmail.com>
2015-04-15 01:33:35 -0700

    add crawler4j and neo4j graph db

--- app/controllers/Analyzer.java
+++ app/controllers/Analyzer.java
@@ -1,18 +1,33 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.url.WebURL;
 import play.libs.Json;
+import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Results;
+import services.SimpleWebCrawler;
+import org.neo4j.graphdb.GraphDatabaseService;
 
 /**
  * Created by hugo on 4/14/15.
  */
-public class Analyzer {
+public class Analyzer extends Controller {
+    public static play.Logger.ALogger logger = play.Logger.of("application.controllers.Analyzer");
+    private static GraphDatabaseService graphDb = null;
+    private static SimpleWebCrawler crawler = new SimpleWebCrawler();
+
     public static Result getMetadata(String url) {
         ObjectNode result = Json.newObject();
 
         result.put("url", url);
+
+
+        WebURL crawlUrl = new WebURL();
+        crawlUrl.setURL("https://www.google.com");
+        crawler.visit(new Page(crawlUrl));
+        logger.debug("crawler initiated!");
 
         return Results.ok(result);
     }
--- build.sbt
+++ build.sbt
@@ -6,9 +6,14 @@ lazy val root = (project in file(".")).enablePlugins(PlayJava)
 
 scalaVersion := "2.11.1"
 
+resolvers ++= Seq(
+  Resolver.sonatypeRepo("snapshots"))
+
 libraryDependencies ++= Seq(
   javaJdbc,
   javaEbean,
   cache,
-  javaWs
+  javaWs,
+  "edu.uci.ics" % "crawler4j" % "4.1",
+  "org.neo4j" % "neo4j" % "2.2.0"
 )
